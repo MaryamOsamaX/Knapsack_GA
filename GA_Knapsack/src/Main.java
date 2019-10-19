5f3125e1ab28cd7b3256ffaf3ca24f1d1be6ad3f
EsraaIB <ibrahimesraa69@gmail.com>
2019-10-19 15:17:27 +0200

    edit main & item constructor

--- GA_Knapsack/src/Main.java
+++ GA_Knapsack/src/Main.java
@@ -1,8 +1,51 @@
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader reader;
+		Vector<Item> items=new Vector<Item>();
+		try {
+			reader = new BufferedReader(new FileReader("src/input_example.txt"));
+			int C = Integer.parseInt(reader.readLine());///num of test cases
+			
+			reader.readLine();
+			
+			for(int i=0 ; i<C; i++)
+			{
+				int N= Integer.parseInt(reader.readLine()); ///num of items
+				int S= Integer.parseInt(reader.readLine());///knapsack size
+				for(int j=0; j<N;j++)
+				{
+					String line=reader.readLine();
+					String []arr=line.split(" ");
+					 
+				items.add(new Item( Integer.parseInt(arr[0]),Integer.parseInt(arr[1])));
+				KnapsackGA ks=new KnapsackGA(N,N,S,items);
+				//call rest of functions 
+				
+				}
+				if(i!=C-1)
+				{
+					reader.readLine();
+					reader.readLine();
+				}
+				
+				
+			}
+			
+		
+			
+			
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	 
+		
 
 	}
 
